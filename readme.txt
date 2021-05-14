35272dcc7fae2ddc3d317c277559e132dea63111
thaakur <avirms07@gmail.com>
2021-05-14 07:22:19 +0530

    initial commit

---
+++ readme.txt
@@ -0,0 +1,5 @@
+docker-compose build
+docker-compose up
+docker ps -a
+localhost:30001
+localhost:30002
---
+++ www/db_connect.php
@@ -0,0 +1,3 @@
+<?php 
+
+$conn= new mysqli('ca-mysql-app','avinashkumar','11812851','ca_db') or die("Could not connect to mysql".mysqli_error($con));
